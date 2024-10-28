26cdaf50d2007cb42f8aa200af39b7b158aaeb23
megasxlr <bjarkigylfa@hotmail.com>
2024-10-28 03:46:11 +0000

    frame rate is done

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from constants import *
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        color = (0, 0, 0)
-        screen.fill(color)
+        screen.fill("black")
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
